e9db24eeec8defc83c849d84a1e521faa1c7b03a
YasOleg <yasoleg94@gmail.com>
2017-03-07 01:04:15 +0200

    Add files via upload

---
+++ Library/Main_LogForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Library
+{
+    partial class Main_LogForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbLogin = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.lbLogin = new System.Windows.Forms.Label();
+            this.lbPassword = new System.Windows.Forms.Label();
+            this.btnLogOn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbLogin
+            // 
+            this.tbLogin.Location = new System.Drawing.Point(117, 70);
+            this.tbLogin.Name = "tbLogin";
+            this.tbLogin.Size = new System.Drawing.Size(100, 20);
+            this.tbLogin.TabIndex = 0;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(117, 108);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(100, 20);
+            this.tbPassword.TabIndex = 1;
+            // 
+            // lbLogin
+            // 
+            this.lbLogin.AutoSize = true;
+            this.lbLogin.Location = new System.Drawing.Point(55, 70);
+            this.lbLogin.Name = "lbLogin";
+            this.lbLogin.Size = new System.Drawing.Size(33, 13);
+            this.lbLogin.TabIndex = 2;
+            this.lbLogin.Text = "Login";
+            // 
+            // lbPassword
+            // 
+            this.lbPassword.AutoSize = true;
+            this.lbPassword.Location = new System.Drawing.Point(58, 111);
+            this.lbPassword.Name = "lbPassword";
+            this.lbPassword.Size = new System.Drawing.Size(53, 13);
+            this.lbPassword.TabIndex = 3;
+            this.lbPassword.Text = "Password";
+            // 
+            // btnLogOn
+            // 
+            this.btnLogOn.Location = new System.Drawing.Point(117, 181);
+            this.btnLogOn.Name = "btnLogOn";
+            this.btnLogOn.Size = new System.Drawing.Size(75, 23);
+            this.btnLogOn.TabIndex = 4;
+            this.btnLogOn.Text = "Ok";
+            this.btnLogOn.UseVisualStyleBackColor = true;
+            this.btnLogOn.Click += new System.EventHandler(this.btnLogOn_Click);
+            // 
+            // Main_LogForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnLogOn);
+            this.Controls.Add(this.lbPassword);
+            this.Controls.Add(this.lbLogin);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbLogin);
+            this.Name = "Main_LogForm";
+            this.Text = "Log_Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbLogin;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label lbLogin;
+        private System.Windows.Forms.Label lbPassword;
+        private System.Windows.Forms.Button btnLogOn;
+    }
+}
+
---
+++ Library/Main_LogForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Client;
+using Librarian;
+using DataBaseApplication;
+
+
+namespace Library
+{
+    public partial class Main_LogForm : Form
+    {
+        public Main_LogForm()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnLogOn_Click(object sender, EventArgs e)
+        {
+            Client_Form frmClient = new Client_Form();
+            Librarian_Form frmLib = new Librarian_Form();
+            DbManager.SearchUser(tbLogin.Text, tbPassword.Text, frmLib, frmClient);
+        }
+    }
+}
